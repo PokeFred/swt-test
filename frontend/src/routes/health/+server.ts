bd2a20336ba92f1f55d97ec1ff0dbf570d1fc6ac
PokeFred <Cedric.paelmke@gmx.de>
2025-04-11 19:20:57 +0200

    update

--- frontend/src/routes/health/+server.ts
+++ frontend/src/routes/health/+server.ts
@@ -1,13 +1,5 @@
 import type { RequestHandler } from "./$types"
 
 export const GET: RequestHandler = async () => {
-    const data: string = JSON.stringify(null)
-
-    return new Response(data, {
-        status: 200,
-        headers: {
-            "Content-Type": " text/plain",
-            "Content-Length": data.length.toString()
-        }
-    })
+    return new Response(null)
 }
